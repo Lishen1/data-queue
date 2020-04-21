79e2f5a59c24f4e7b3aeb298d2d3b2fdf112b01e
Pantelyuk Pavel <panteluk@nkbvs.ru>
2020-04-21 20:29:24 +0300

    add basic search

--- types.h
+++ types.h
@@ -1,1 +1,15 @@
 #pragma once
+
+namespace daqu
+{
+  template <typename dataT, typename timeT>
+  struct stamped_data
+  {
+    stamped_data(const dataT& d, const timeT& t) : data(d), ts(t) {}
+    stamped_data() : data(dataT()), ts(timeT()) {}
+
+    dataT data;
+    timeT ts;
+  };
+
+} // namespace daqu
